73da6614029bde948f9f819109dc6e439a5caff0
Jacob Mortensen <jacob.w.mortensen@gmail.com>
2015-03-19 12:52:56 -0600

    Updated Presentation Materials

--- assignments/spatial 911/figures.R
+++ assignments/spatial 911/figures.R
@@ -4,15 +4,28 @@ source("spatial911.R")
 ndraws <- 1000
 
 draws <- mh.gibbs(ndraws, 1, 0.01, 0.01)
-burnin <- 20000
+burnin <- 1000
 lambda.post.mn <- apply(draws$lambda[-c(1:burnin),], 2, mean)
+lambda.post.low <- apply(draws$lambda[-c(1:burnin),], 2, quantile, probs=c(0.025))
+lambda.post.high <- apply(draws$lambda[-c(1:burnin),], 2, quantile, probs=c(0.975))
 plot.grid <- matrix(NA, nrow=125, ncol=125)
+low.grid <- matrix(NA, nrow=125, ncol=125)
+high.grid <- matrix(NA, nrow=125, ncol=125)
 plot.grid[kp.gp] <- lambda.post.mn
+low.grid[kp.gp] <- lambda.post.low
+high.grid[kp.gp] <- lambda.post.high
 
 pdf("posteriorLocationDensity.pdf")
 image.plot(x.grid, y.grid, plot.grid, axes=FALSE, frame.plot=TRUE, ann=FALSE,
-  xlim=c(-95.79, -95.0), ylim=c(29.5, 30.15)) 
-plot(houston, add=TRUE)
+  xlim=c(-95.79, -95.0), ylim=c(29.5, 30.15))#, zlim=c(0, 0.012)) 
+dev.off()
+pdf("posteriorLow.pdf")
+image.plot(x.grid, y.grid, low.grid, axes=FALSE, frame.plot=TRUE, ann=FALSE,
+  xlim=c(-95.79, -95.0), ylim=c(29.5, 30.15), zlim=c(0, 0.012))
+dev.off()
+pdf("posteriorHigh.pdf")
+image.plot(x.grid, y.grid, high.grid, axes=FALSE, frame.plot=TRUE, ann=FALSE,
+  xlim=c(-95.79, -95.0), ylim=c(29.5, 30.15), zlim=c(0, 0.014))
 dev.off()
 
 delta.density <- data.frame(x=density(draws$delta[-c(1:burnin)])$x, y=density(draws$delta[-c(1:burnin)])$y)
